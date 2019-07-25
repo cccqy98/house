98d158ee33bd23992a3aecf11690d202050b9a36
ck <945787475@qq.com>
2019-07-25 21:12:29 +0800

    曹康测试

---
+++ src/main/java/com/aaa/house/controller/AMan.java
@@ -0,0 +1,13 @@
+package com.aaa.house.controller;
+
+/**
+ * FileName: AMan
+ * Author:   曹康
+ * Date:     2019/7/25 21:07
+ * Description:
+ */
+public class AMan {
+    public static void main(String[] args) {
+        System.out.println("sdfsd");
+    }
+}
